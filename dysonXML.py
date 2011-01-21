0cafdd13c78b9bd8da50da2d1967547fdf20e124
tservi <jeantinguelyawais@gmail.com>
2011-01-21 13:11:08 +0000

    first step : putting the background!

--- dysonXML.py
+++ dysonXML.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 
+# python 3 
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as etree
+
+url     = 'http://vk.stnet.ch/XmlEventList.jsp?lang=fr&maxrows=50000&cat=B&dateinterval=7000&rlevel=nat&fields=all'
+page    = urllib.request.urlopen( url )
+#print( page.getheaders() )
+content = page.read( ).decode( 'WINDOWS-1252' ).replace( '\r\n' , '' ).replace( '\n' , '' ).replace( '\t' , '' )
+#print( content )
+tree = etree.fromstring( content )
+for child in tree:
+    print( child )
